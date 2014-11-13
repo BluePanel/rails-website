47d7e6e140df9837ba5e427836ee7dbe2570d9a4
Marcel Wiegand <marcel@bluepanel.org>
2014-11-13 12:33:37 +0100

    Check admin rights in ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,5 +28,11 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+
+    if user.has_role? :admin
+      can :manage, :all
+    else
+      can :read, :all
+    end
   end
 end
