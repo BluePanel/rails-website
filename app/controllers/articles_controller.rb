06782c823d0eec3e0c7b3c7070adddb639f7299a
Marcel Wiegand <marcel@bluepanel.org>
2014-11-13 22:23:56 +0100

    Add authentication and authorization for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   respond_to :html
 
