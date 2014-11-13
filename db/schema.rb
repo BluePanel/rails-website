968cf29fe849a7e7852a76a1bb2ac618e9c2cee2
Marcel Wiegand <marcel@bluepanel.org>
2014-11-13 13:35:26 +0100

    Run migration for article

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141109225514) do
+ActiveRecord::Schema.define(version: 20141113122555) do
+
+  create_table "articles", force: true do |t|
+    t.integer  "author"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
