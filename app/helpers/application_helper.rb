5298df1c9edbdef766deb758984702db83796eb5
Marcel Wiegand <marcel@bluepanel.org>
2014-12-08 00:43:55 +0100

    Create helpers for admin and/or moderator approval

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_user_is_admin?
+    user_signed_in? && (current_user.has_role? :admin)
+  end
+
+  def current_user_is_mod?
+    user_signed_in? && (current_user.has_role? :moderator)
+  end
+
+  def current_user_is_mod_or_admin?
+    current_user_is_mod? || current_user_is_admin?
+  end
 end
