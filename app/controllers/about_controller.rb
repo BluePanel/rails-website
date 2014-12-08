ae94e97142862b52296a5fd9a1e6a29277f8c1cc
Marcel Wiegand <marcel@bluepanel.org>
2014-12-08 03:20:02 +0100

    List admins, global mods and developers on about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,7 @@
 class AboutController < ApplicationController
   def show
+    @admins = User.with_role :admin
+    @mods = User.with_role :moderator
+    @team = User.with_role :developer
   end
 end
