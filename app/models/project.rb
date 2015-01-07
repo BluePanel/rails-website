e31fc0152272222c4cba9511c7ca5f4fd079ae58
Marcel Wiegand <marcel@bluepanel.org>
2015-01-07 11:12:46 +0100

    Localisation of project description in views. Fixes #22

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,11 @@
 class Project < ActiveRecord::Base
   resourcify
+
+  def localized_description
+    if I18n.locale == :de
+      self.description_de
+    else
+      self.description # default (English)
+    end
+  end
 end
