91d75a2a15ce6e651a398cf025b8fc846857c822
Marcel Wiegand <marcel@bluepanel.org>
2015-02-02 03:17:55 +0100

    Add default_scope order (created_at desc) to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ActiveRecord::Base
 
   belongs_to :author, :class_name => 'User', :foreign_key => :author_id
 
+  default_scope { order('created_at DESC') }
+
   def content_preview
       content.truncate(BP_CONFIG['article_preview_sign_length'])
   end
