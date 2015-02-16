b9a61815e53c9ba7b006b53393e88a77f215b65d
Marcel Wiegand <marcel@bluepanel.org>
2015-02-16 21:21:01 +0100

    Add admin user and BluePanel project to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create admin user
+admin_user = User.new
+admin_user.name = 'Admin'
+admin_user.email = 'admin@localhost.localhost'
+admin_user.password = '12345678'
+admin_user.password = '12345678'
+admin_user.add_role :admin
+admin_user.save!
+
+# create BluePanel.org project
+project = Project.new
+project.name = 'BluePanel Website'
+project.description = 'The online presence of BluePanel.org'
+project.description_de = 'Die Webpräsenz von BluePanel.org'
+project.link = 'https://github.com/BluePanel/rails-website'
+project.save!
+
