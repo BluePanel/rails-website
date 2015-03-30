66f5955bf82e6148e626cd782aac37f6a7c0cfb5
CemeBP <lukas@bluepanel.org>
2015-03-30 03:50:04 +0200

    Change html structure of show project view and add bb-code and html formating

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,12 @@ class Project < ActiveRecord::Base
   resourcify
   translates :description, :fallbacks_for_empty_translations => true
 
+  def description_preview
+    description.truncate(BP_CONFIG['project_preview_sign_length']).bbcode_to_html_with_formatting unless description.nil?
+  end
+
+  def description_with_bbcode_and_formatting
+    description.bbcode_to_html_with_formatting unless description.nil?
+  end
+
 end
